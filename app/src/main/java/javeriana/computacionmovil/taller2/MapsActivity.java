ae22cc8d8946b12d09ccc8c794ff4bc287df5e1e
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2020-03-31 22:54:50 -0500

    punto 6,7,8 y 9 hechos por pablo ventemilla listos

--- app/src/main/java/javeriana/computacionmovil/taller2/MapsActivity.java
+++ app/src/main/java/javeriana/computacionmovil/taller2/MapsActivity.java
@@ -4,14 +4,22 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -25,8 +33,13 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     SensorManager sensorManager;
@@ -36,7 +49,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     Location currentLocation;
     FusedLocationProviderClient fusedLocationProviderClient;
+    Location destiny;
+    Geocoder mGeocoder;
+    TextView mAddress;
+
     private static final int REQUEST_CODE = 101;
+    public static final double lowerLeftLatitude = 1.396967;
+    public static final double lowerLeftLongitude= -78.903968;
+    public static final double upperRightLatitude= 11.983639;
+    public static final double upperRigthLongitude= -71.869905;
+    private static final double RADIUS_OF_EARTH_KM = 6371;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +67,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         sensorManager= (SensorManager) getSystemService(SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         fetchLocation();
+        destiny = new Location("");
+        mGeocoder = new Geocoder(getBaseContext());
+
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         lightSensorListener = new SensorEventListener() {
@@ -115,6 +140,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
         googleMap.addMarker(markerOptions);
+        direccion(latLng);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(mAddress.getWindowToken(), 0);
+            }
+        });
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                try {
+                    double d = distance(currentLocation.getLatitude(), currentLocation.getLongitude(), latLng.latitude, latLng.longitude);
+                    mMap.clear();
+                    destiny.setLatitude(latLng.latitude);
+                    destiny.setLongitude(latLng.longitude);
+                    Toast.makeText(MapsActivity.this, "Estas a " + d + " de tu destino", Toast.LENGTH_SHORT).show();
+                    mMap.addMarker(new MarkerOptions().position(latLng)
+                            .title(mGeocoder.getFromLocation(latLng.latitude, latLng.longitude, 1).get(0).getAddressLine(0)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         }
     @Override
@@ -123,7 +173,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             case REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     //SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);mapFragment.getMapAsync(this);
+
                     fetchLocation();
+
+                    //
                 }
                 break;
         }
@@ -137,6 +190,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(lightSensorListener);
+    }
+    public double distance(double lat1, double long1, double lat2, double long2) {
+        double latDistance = Math.toRadians(lat1 - lat2);
+        double lngDistance = Math.toRadians(long1 - long2);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lngDistance / 2) * Math.sin(lngDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double result = RADIUS_OF_EARTH_KM * c;
+        return Math.round(result*100.0)/100.0;
+    }
+
+    public void direccion(final LatLng latLng){
+        mAddress =findViewById(R.id.texto);
+        mAddress.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE || event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    String addressString = mAddress.getText().toString();
+                    if (!addressString.isEmpty()) {
+                        try {
+                            List<Address> addresses = mGeocoder.getFromLocationName( addressString, 2,
+                                    lowerLeftLatitude, lowerLeftLongitude,
+                                    upperRightLatitude, upperRigthLongitude);
+                            if (addresses != null && !addresses.isEmpty()) {
+                                Address addressResult = addresses.get(0);
+                                LatLng position = new LatLng(addressResult.getLatitude(), addressResult.getLongitude());
+                                if (mMap != null) {
+                                    MarkerOptions myMarkerOptions = new MarkerOptions();
+                                    myMarkerOptions.position(position);
+                                    myMarkerOptions.title(addressString);
+                                    myMarkerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE));
+                                    mMap.clear();
+                                    destiny.setLatitude(addressResult.getLatitude());
+                                    destiny.setLongitude(addressResult.getLongitude());
+                                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                                    imm.hideSoftInputFromWindow(mAddress.getWindowToken(), 0);
+                                    mAddress.setText("");
+                                    mMap.addMarker(myMarkerOptions);
+                                    double d = distance(latLng.latitude, latLng.longitude,
+                                            destiny.getLatitude(), destiny.getLongitude());
+                                    try {
+                                        Polyline line =  mMap.addPolyline(new PolylineOptions().add(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude())
+                                                , new LatLng(destiny.getLatitude(), destiny.getLongitude()))
+                                                .width(5)
+                                                .color(Color.RED));
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    Toast.makeText(MapsActivity.this, "Estas a " +d+ " de tu destino" , Toast.LENGTH_SHORT).show();
+                                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 14));
+                                }
+                            } else {Toast.makeText(MapsActivity.this, "Dirección no encontrada", Toast.LENGTH_SHORT).show();}
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {Toast.makeText(MapsActivity.this, "La dirección esta vacía", Toast.LENGTH_SHORT).show();}
+                }
+                return false;
+            }
+        });
+
     }
 
 }
